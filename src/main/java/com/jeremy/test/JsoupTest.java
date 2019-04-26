cdbb91ef7e6b50fa3a3a582c6b13c91d5a6e9737
wangqian <chenjun@toroot.cn>
2019-04-26 17:20:38 +0800

    代码修改

--- src/main/java/com/jeremy/test/JsoupTest.java
+++ src/main/java/com/jeremy/test/JsoupTest.java
@@ -17,19 +17,22 @@ public class JsoupTest {
 
 
     public static void main(String[] args) throws Exception {
-        Document document = Jsoup.connect("http://www.astronomerstelegram.org/?read=11399").get();
+        Document document = Jsoup.connect("http://www.astronomerstelegram.org/?read=12685").get();
         // 爬取电文序号
         String numberName = document.select("#telegram > p:nth-child(3)").text();
-
+        // 获取电文需要
         String messageNumber = subNumber(numberName);
-        System.out.println("电文序号"+messageNumber);
+
         // 获取标题和正文
         String content = document.select("#telegram").text();
+
+        // 爬取爆发源名称
+        String burstName = subBurstName(content);
         // 爬取观测设备
         String device = subDevice(content);
 
-
-
+        // 获取爆发源类型
+        String burstTypeName = subBurstType(content);
 
 
     }
@@ -58,9 +61,9 @@ public class JsoupTest {
     //
 
 
-
     /**
      * 获取观测地址
+     *
      * @param deviceName
      * @return
      */
@@ -78,44 +81,10 @@ public class JsoupTest {
         return device;
     }
 
-    /**
-     *
-     *
-     * @param deviceName
-     * @return
-     */
-   /* static List<String> subDevice(String deviceName) {
-        String[] deviceArr = {"HXMT", "ATCA", "MAXI/GSC"};
-        List<String> valueList = new ArrayList<>();
-        for (int i = 0; i < deviceArr.length; i++) {
-            name(deviceName, deviceArr[i], valueList);
-        }
-        return valueList;
-    }*/
-
-    /**
-     * 递归调用自己
-     *
-     * @param subName
-     * @param name
-     * @param valueList
-     * @return
-     */
-   /* static List<String> name(String subName, String name, List<String> valueList) {
-        if (subName.contains(name)) {
-            int indexValue = subName.indexOf(name);
-            // 获取截取的值
-            String subValue = subName.substring(indexValue, indexValue + name.length());
-            valueList.add(subValue);
-            String subNext = subName.substring(indexValue + name.length());
-            name(subNext, name, valueList);
-        }
-        return valueList;
-
-    }*/
 
     /**
      * 获取爆发源
+     *
      * @param burstSourceName
      * @return
      */
@@ -136,37 +105,22 @@ public class JsoupTest {
     }
 
 
-   /* static String subBurstLocation(String burstSourceLocation) {
-        String locationName = null;
-        int index = 0 ;
-        String [] location = {"R.A.", "Decl.", "RA", "Dec.", "(RA, Dec)"};
-        for (int i = 0; i < location.length; i++) {
-            if (burstSourceLocation.contains(location[i])) {
-                int indexValue = burstSourceLocation.indexOf(location[i]);
-                String equalsSrt = burstSourceLocation.substring(indexValue);
-                // 判断如果这个截取到的字符串包含等于，那么表名位置有效
-                if (equalsSrt.contains("=")) {
-                    int equalIndexValue = equalsSrt.indexOf("=");
-                    String locationStr = equalsSrt.substring(equalIndexValue, equalIndexValue+40);
-                    for (int j = locationStr.length(); --j >= 0;) {
-                        int chr = locationStr.charAt(j);
-                        // 判断最后一位数是数字的索引值
-                        if (chr < 48 || chr > 57) {
-                              index = j-1;
-                              continue;
-                        }
-                    }
-                    // 在截
-                    locationName  =  locationStr.substring(0,index);
-                }
-            }
-        }
-        return locationName;
-    }*/
-
-    /*static String subBurstType(String burstType) {
+    /**
+     * 判断是否是新源还是已知源
+     *
+     * @param burstType
+     * @return
+     */
+    static String subBurstType(String burstType) {
+        String buType = null;
         String type = "uncatalogued X-ray transient source";
-    }*/
+        if (burstType.contains(type)) {
+            buType = "新源";
+        } else {
+            buType = "已知源";
+        }
+        return buType;
+    }
 
 
 }
